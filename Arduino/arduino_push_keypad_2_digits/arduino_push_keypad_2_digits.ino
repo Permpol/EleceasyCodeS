35983e73103b9ad2834d1ee2ccf169fd176c7084
Remy Martin <moneyp10000@gmail.com>
2020-07-06 21:10:42 +0700

    เพิ่มการเช็ค

--- Arduino/arduino_push_keypad_2_digits/arduino_push_keypad_2_digits.ino
+++ Arduino/arduino_push_keypad_2_digits/arduino_push_keypad_2_digits.ino
@@ -8,58 +8,81 @@ const byte COLS = 3; //three columns
 int inputA1;
 
 char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
-};
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
 byte rowPins[ROWS] = {8, 7, 6, 5}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {4, 3, 2}; //connect to the column pinouts of the keypad
+byte colPins[COLS] = {4, 3, 2};    //connect to the column pinouts of the keypad
 
 //Create an object of keypad
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-void setup(){
-  pinMode(9, OUTPUT);
-TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(COM1B0) | _BV(WGM11); 
-TCCR1B = _BV(WGM13) | _BV(CS11);
-  
-  Serial.begin(9600);
+void setup()
+{
+    pinMode(9, OUTPUT);
+    TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(COM1B0) | _BV(WGM11);
+    TCCR1B = _BV(WGM13) | _BV(CS11);
+
+    Serial.begin(9600);
 }
 
 char key;
 int ckey = 0;
-void loop(){  
- 
-
-  key = keypad.getKey();// Read the key
-
-if (key != NO_KEY)
+String varKey1 = "";
+String varKey2 = "";
+void loop()
 {
-Serial.println(key);
-}
 
-if(ckey == 2){
-  if(key == '0'){ //เมื่อ กด 0 เข้าเงื่อนไขนี้
-      inputA1 = 1024; // แทนค่า inputA1 ด้วย 1024 ที่ทำให้ PWM มี dutycycle 5 %
-      Serial.println(String("Read inputA1 = ")+ inputA1);
-  }
-  if(key == '1'){ //เมื่อ กด 1 เข้าเงื่อนไขนี้
-      inputA1 = 900; // แทนค่า inputA1 ด้วย 900 ที่ทำให้ PWM มี dutycycle 10 %
-      Serial.println(String("Read inputA1 = ")+ inputA1);
-  }
+    while (ckey == 0)
+    {
+        key = keypad.getKey(); // Read the key
 
+        if (key != NO_KEY)
+        {
+            Serial.println(key);
+            varKey1 = String(key);
+            ckey++;
+        }
+    }
 
- ICR1 = 62.5; 
-pwm1 = inputA1; 
+    while (ckey == 1)
+    {
+        key = keypad.getKey(); // Read the key
 
-xxx = float(pwm1);
+        if (key != NO_KEY)
+        {
+            Serial.println(key);
+            varKey2 = String(key);
+            ckey++;
+        }
+    }
 
-xxx = xxx * ICR1;
+    if (ckey == 2)
+    {
+        String KeySum = varKey1 + varKey2;
+        Serial.println(String("KeySum = ") + KeySum);
+        if (KeySum == "15")
+        {                  //เมื่อ กด 0 เข้าเงื่อนไขนี้
+            inputA1 = 950; // แทนค่า inputA1 ด้วย 1024 ที่ทำให้ PWM มี dutycycle 5 %
+            Serial.println(String("Read inputA1 = ") + inputA1);
+        }
+        if (KeySum == "20")
+        {                  //เมื่อ กด 1 เข้าเงื่อนไขนี้
+            inputA1 = 900; // แทนค่า inputA1 ด้วย 900 ที่ทำให้ PWM มี dutycycle 10 %
+            Serial.println(String("Read inputA1 = ") + inputA1);
+        }
 
-xxx = xxx / 512;
+        ICR1 = 62.5;
+        pwm1 = inputA1;
 
-OCR1A = int(62.5-(xxx/2));
+        xxx = float(pwm1);
 
-  }
+        xxx = xxx * ICR1;
+
+        xxx = xxx / 512;
+
+        OCR1A = int(62.5 - (xxx / 2));
+        ckey = 0;
+    }
 }
