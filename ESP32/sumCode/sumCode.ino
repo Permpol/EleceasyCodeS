efcaa3f088f83958b223f4982e18d4e13d98c240
Permpolthan <moneyp1000@gmail.com>
2022-09-22 18:24:35 +0700

    หลังรวม

--- ESP32/sumCode/sumCode.ino
+++ ESP32/sumCode/sumCode.ino
@@ -4,9 +4,19 @@
 #define SSID        "******"
 #define PASSWORD    "******"
 #define LINE_TOKEN  "*************"
+#define LED 2
+
 const char* ntpServer = "pool.ntp.org"; 
 const long  gmtOffset_sec = 0 * 3600;
 const int   daylightOffset_sec = 7 * 3600;
+
+bool Normal = false;
+bool High = false;
+bool Low = false;
+bool Off = false;
+unsigned long period =  3600000 * 12ul ;
+unsigned long last_time = 0;
+PZEM004Tv30 pzem(Serial1, 16, 17);
 
 void printLocalTime()
 
@@ -20,19 +30,9 @@ void printLocalTime()
  }
 
 
-bool Normal = false;
-bool High = false;
-bool Low = false;
-bool Off = false;
-unsigned long period =  3600000 * 12ul ;
-unsigned long last_time = 0;
+void setup() {  
 
-
-
-PZEM004Tv30 pzem(Serial1, 16, 17);
-#define LED 2
-
-void setup() {  pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(115200); 
   Serial.println();
   Serial.println(LINE.getVersion());
@@ -44,24 +44,46 @@ void setup() {  pinMode(LED, OUTPUT);
     delay(400);
    }
             
-   Serial.println(" CONNECTED");
-   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-   printLocalTime();
-      
-  
-       
+  Serial.println(" CONNECTED");
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime(); 
   Serial.printf("\nWiFi connected\nIP : ");
   Serial.println(WiFi.localIP());
   LINE.setToken(LINE_TOKEN);
   LINE.notify("Node..WatKhaoDinNongSaeng-Site");
   delay(500);
+
+  Serial.begin(115200);
+
+  // delete old config
+  WiFi.disconnect(true);
+
+  delay(1000);
+
+  WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+
+  /* Remove WiFi event
+  Serial.print("WiFi Event ID: ");
+  Serial.println(eventID);
+  WiFi.removeEvent(eventID);*/
+
+  WiFi.begin(ssid, password);
+    
+  Serial.println();
+  Serial.println();
+  Serial.println("Wait for WiFi... ");
+  
 }
 
 void loop() {
+
   delay(500);
   digitalWrite(LED, HIGH);
   delay(500);
   digitalWrite(LED, LOW);
+
   if ( millis() - last_time > period) {
     Normal = false;
     Low = false;
@@ -118,4 +140,21 @@ void loop() {
 
   Serial.println();
   delay(1500);
+}
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Connected to AP successfully!");
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Disconnected from WiFi access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.disconnected.reason);
+  Serial.println("Trying to Reconnect");
+  WiFi.begin(ssid, password);
 }
